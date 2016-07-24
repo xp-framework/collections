d458a8401a61fa20af9b054deb76c7a57d81bdf2
Timm Friebe <timm.friebe@1und1.de>
2016-07-24 23:30:04 +0200

    Suppress "A non well formed numeric value encountered" warning

--- src/main/php/util/collections/HashProvider.class.php
+++ src/main/php/util/collections/HashProvider.class.php
@@ -36,6 +36,7 @@ class HashProvider extends \lang\Object {
     // hex numbers larger than LONG_MAX. Use 2^64 + 1 as hex literal and see if
     // it's "truncated", using the slower hexdec(md5()) implementation then.
     if (PHP_INT_MAX === @0x20c49ba5e35423 || 0 === "0x1" + 0) {
+      \xp::gc(__FILE__, __LINE__ - 1);
       $impl= \lang\XPClass::forName('util.collections.MD5HexHashImplementation')->newInstance();
     } else {
       $impl= new MD5HashImplementation();
